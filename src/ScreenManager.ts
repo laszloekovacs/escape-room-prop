2773fcc9044a084bd0ff08a82deeaeb1ae92e6ff
laszloekovacs <laszloekovacs@gmail.com>
2025-03-08 17:12:12 +0100

    added readline to prevent exiting

--- src/ScreenManager.ts
+++ src/ScreenManager.ts
@@ -1,11 +1,19 @@
 import type { Screen } from './Screen'
+import readline from 'node:readline'
 
 export class ScreenManager {
 	private static instance: ScreenManager
 	private screen: Screen
+	private rl: readline.Interface
 
 	private constructor(initialScreen: Screen) {
 		this.screen = initialScreen
+
+		// this will prevent the process to exit
+		this.rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		})
 	}
 
 	public static getInstance(initialScreen: Screen): ScreenManager {

4ad2f093517e6c8663949c34a08bd31c3bce6c81
laszloekovacs <laszloekovacs@gmail.com>
2025-03-08 19:47:46 +0100

    added python solutions

---
+++ img.py
@@ -0,0 +1,8 @@
+import climage 
+
+# converts the image to print in terminal 
+# inform of ANSI Escape codes 
+output = climage.convert('640.png', width=10) 
+
+# prints output on console. 
+print(output)

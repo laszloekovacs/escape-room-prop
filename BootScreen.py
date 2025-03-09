4ce2e0d49841a751110c696a9f0a43c9bccc7ad8
laszloekovacs <laszloekovacs@gmail.com>
2025-03-09 17:49:21 +0100

    refactored boot screen

--- BootScreen.py
+++ BootScreen.py
@@ -13,21 +13,24 @@ class BootScreen(Screen):
         print(term.home + term.clear)
         
         with(term.cbreak(), term.hidden_cursor()):
+            print(term.move_x(5) + "T: 1982.03.22 - 20:13")
             print(term.move_xy(5, 2) + "rendszer indítása\n")
             time.sleep(1)
-            print(term.move_x(5) + "ram")
-            print(term.move_x(5) + "64kb")
-            print(term.move_x(5) + "rom")
-            print(term.move_x(5) + "256kb\n")
-            time.sleep(1)
-            print(term.move_x(5) + "hálózat")
-            print(term.move_x(5) + "ok\n")
-            print(term.move_x(5) + "terminál")
-            print(term.move_x(5) + "ok\n")
-            time.sleep(0.4)
-            print(term.move_x(5) + "kgb megfigyelő")
-            print(term.move_x(5) + "csatlakozás...")         
+            print(term.move_x(5) + "\tRAM\t\tROM\t\tHDD")
+            print(term.move_x(5) + "\t64kb\t\t256K\t\t24Mb\n")
             
             time.sleep(1)
+            print(term.move_x(5) + "hálózat")
+            print(term.move_x(5) + "ok")
+            print(term.move_x(5) + "terminál")
+            print(term.move_x(5) + "ok\n")
+            
+            time.sleep(0.4)
+            print(term.move_x(5) + "kgb megfigyelő hálózat")
+            print(term.move_x(5) + "csatlakozás sikertelen\n")
+
+            print(term.move_x(5) + "bejelentkezés...")
+            
+            time.sleep(1.8)
         
         self.manager.transition(None)
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from Manager import Manager
 if __name__ == '__main__':
     app = Manager()
     app.run()
+

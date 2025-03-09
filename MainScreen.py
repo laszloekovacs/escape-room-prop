bf24c5b1435a52bac9ab2eaf3ecc9a1e0c7a2c4e
laszloekovacs <laszloekovacs@gmail.com>
2025-03-09 18:50:35 +0100

    added horizontal menu

--- MainScreen.py
+++ MainScreen.py
@@ -1,27 +1,52 @@
+from blessed import Terminal
+from Screen import Screen
+from term_image.image import from_file
+
 # main menu. this should render a logo and a menu to select submenus
 
-from blessed import Terminal
 
-from Screen import Screen
 
 term = Terminal()
 
 # simulates standby state, ask user to press key for booting
 # extends Screen
 class MainScreen(Screen):
+    options = ["keresés", "jelentések"]
+    selection = 0
+
     def __init__(self, manager):
         super().__init__(manager)
 
 
     def render(self):
-        print("main")
+        with term.cbreak(), term.hidden_cursor():
+            print(term.home + term.clear)
 
-        with(term.cbreak(), term.hidden_cursor()):
-            term.inkey()
-            print("indítás...")
-            self.manager.transition(None)
-            
-        
-        
+            image = from_file("hand.jpg")
+            print(image)
+            #print(term.move_y(term.height) + "hello")
+
+            menuline = ""
+
+            for i, option in enumerate(self.options):
+                if i == self.selection:
+                    menuline += term.reverse(option) + " "
+                else:
+                    menuline += option + " "
+                
+            print(term.move_y(term.height-1) + menuline)
+                    
+            key = term.inkey()
+
+            if key:
+                if key.name == "KEY_LEFT":
+                    self.selection = (self.selection - 1) % len(self.options)
+                elif key.name == "KEY_RIGHT":
+                    self.selection = (self.selection + 1) % len(self.options)
+
+                elif key.name == "KEY_ESCAPE":
+                    print("exiting")
+                    self.manager.transition(None)
+                    
         
 

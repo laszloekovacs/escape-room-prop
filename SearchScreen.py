d058d7cd1ea55ac4d8809a3fcaa9dd09e65e9e62
laszloekovacs <laszloekovacs@gmail.com>
2025-03-21 09:54:46 +0100

    fixed typo

--- SearchScreen.py
+++ SearchScreen.py
@@ -12,7 +12,7 @@ class SearchScreen(Screen.Screen):
 
     def render(self):
         print(term.home + term.clear)
-        prompt = input(f"{term.move_x(term.width // 2) + term.move_y(term.height // 2)}kereses: ")
+        prompt = input(f"{term.move_x(term.width // 2) + term.move_y(term.height // 2)}keresés: ")
 
         if prompt:
             self.manager.transition(SearchPendingScreen.SearchPendingScreen(self.manager, prompt))
